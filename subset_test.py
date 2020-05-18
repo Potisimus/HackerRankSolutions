83fbb912a01c7d38a7d38d479816b7547b7006c5
Potisiums <pal.uddin@gmail.com>
2020-05-18 11:06:48 -0400

    HR solution for subset Test

---
+++ subset_test.py
@@ -0,0 +1,44 @@
+'''
+You are given two sets, A and B.
+Your job is to find whether set A is a subset of set B.
+
+If set A is subset of set B, print True.
+If set A is not a subset of set B , print False.
+
+nput Format
+
+The first line will contain the number of test cases, T.
+The first line of each test case contains the number of elements in set A.
+The second line of each test case contains the space separated elements of set A.
+The third line of each test case contains the number of elements in set B.
+The fourth line of each test case contains the space separated elements of set B.
+
+Sample Input
+
+3
+5
+1 2 3 5 6
+9
+9 8 5 6 3 2 1 4 7
+1
+2
+5
+3 6 5 4 1
+7
+1 2 3 5 6 8 9
+3
+9 8 2
+
+Sample Output
+
+True
+False
+False
+
+'''
+
+
+number_of_test = int(input())
+for i in range(number_of_test):
+    item_number_a, set_a, item_number_b, set_b = input(), set(input().split()), input(), set(input().split())
+    print(set_a.issubset(set_b))
